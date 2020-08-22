5119eeb1591b6b28f69c994c3d95b24332603a90
John Carr <johnjcarr17@gmail.com>
2020-08-22 09:35:47 -0400

    Added mitsuba decoder files

--- src/lib/bms.js
+++ src/lib/bms.js
@@ -1,22 +1,22 @@
-
-let helper = require('./helper')
+let helper = require("./helper");
 
 /**
  * Check to see if BMS data was transmitted and return a boolean
- * 
- * @param {number} address 
+ *
+ * @param {number} address
  * @param {number[]} data
  * @returns {Boolean}
  */
 exports.check = function check(address, data) {
-    return helper.addressCheck(
-        address == helper.TELEMETRY_ADDRESS.BMS && data[7] && data[6],
-        () => {
-            helper.sendData("bms", {
-                "LowCellVoltage"  : helper.getWord(data[1], data[0])/100,
-                "highCellVoltage" : helper.getWord(data[3], data[2])/100,
-                "avgCellVoltage"  : helper.getWord(data[5], data[4])/100,
-                "packSumVoltage"  : helper.getWord(data[7], data[6])/100
-            })
-        })
-}
+  return helper.addressCheck(
+    address == helper.TELEMETRY_ADDRESS.BMS && data[7] && data[6],
+    () => {
+      helper.sendData("bms", {
+        LowCellVoltage: helper.getWord(data[1], data[0]) / 100,
+        highCellVoltage: helper.getWord(data[3], data[2]) / 100,
+        avgCellVoltage: helper.getWord(data[5], data[4]) / 100,
+        packSumVoltage: helper.getWord(data[7], data[6]) / 100,
+      });
+    }
+  );
+};
