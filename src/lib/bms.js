57ffb769a9d6969b4583f528110968659a191ff2
Muhamed Hobi <woohoomoo2u@gmail.com>
2021-02-20 05:40:23 -0500

    BMS: correct data field name

--- src/lib/bms.js
+++ src/lib/bms.js
@@ -9,7 +9,8 @@ let helper = require('./helper')
  * @returns {Boolean}
  */
 exports.check = function check(address, data) {
-    if(address == helper.TELEMETRY_ADDRESS.BMS){
+    if(address == helper.TELEMETRY_ADDRESS.BMS && data[7] && data[6]){
+        console.log("BMS: ")
         console.log(helper.getWord(data[1], data[0])/100)
         console.log(helper.getWord(data[3], data[2])/100)
         console.log(helper.getWord(data[5], data[4])/100)
@@ -19,7 +20,7 @@ exports.check = function check(address, data) {
         address == helper.TELEMETRY_ADDRESS.BMS && data[7] && data[6],
         () => {
             helper.sendData("bms", {
-                "LowCellVoltage"  : helper.getWord(data[1], data[0])/100,
+                "lowCellVoltage"  : helper.getWord(data[1], data[0])/100,
                 "highCellVoltage" : helper.getWord(data[3], data[2])/100,
                 "avgCellVoltage"  : helper.getWord(data[5], data[4])/100,
                 "packSumVoltage"  : helper.getWord(data[7], data[6])/100
